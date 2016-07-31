e050444bcf70ab2b3b87a5f3d071ad432b61262b
advaithmobius <advaith@mobiusworks.in>
2016-07-31 11:16:37 +0530

    first commit

---
+++ Tutorial/HelloWorld.py
@@ -0,0 +1,38 @@
+import tensorflow as tf 
+
+# Create a Constant op that produces a 1x2 matrix.  The op is
+# added as a node to the default graph.
+#
+# The value returned by the constructor represents the output
+# of the Constant op.
+
+matrix1 = tf.constant([[3.,3.]])
+
+# Create another Constant that produces a 2x1 matrix.
+
+matrix2 = tf.constant([[2.],[2.]])
+
+# Create another Constant that produces a 2x1 matrix.
+
+product = tf.matmul(matrix1,matrix2)
+
+# Launch the default graph.
+sess = tf.Session()
+
+# To run the matmul op we call the session 'run()' method, passing 'product'
+# which represents the output of the matmul op.  This indicates to the call
+# that we want to get the output of the matmul op back.
+#
+# All inputs needed by the op are run automatically by the session.  They
+# typically are run in parallel.
+#
+# The call 'run(product)' thus causes the execution of threes ops in the
+# graph: the two constants and matmul.
+#
+# The output of the op is returned in 'result' as a numpy `ndarray` object.
+
+result = sess.run(product)
+
+print result
+
+sess.close()
